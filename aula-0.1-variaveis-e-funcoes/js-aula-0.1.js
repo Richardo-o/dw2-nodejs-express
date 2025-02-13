4b983f2d2cc35dafe56a00a65c0c391eb97215d0
Richardo-o <rickysilva04205@gmail.com>
2025-02-13 18:48:56 -0300

    Variáveis e funções o JS

---
+++ aula-0.1-variaveis-e-funcoes/js-aula-0.1.js
@@ -0,0 +1,21 @@
+// Comentário em JS
+/* Comentário
+em bloco ... */
+// CTRL + ; - Atalho
+
+// Declaração de variáveis no JS
+// CONST x LET x VAR
+
+const nome = 'Ricardo'; // Uma constante precisa de um valor inicial
+let Nome = 'Ricardo'; // Permite iniciar a variável vazia
+
+// cosnt name = 'ricardo' -> Resulta em um erro.
+// let Nome -> Resulta em um erro.
+
+var cor = 'Azul';
+var cor = 'Amarelo';
+let cidade = 'Registro';
+cidade = 'Jacupiranga'; // Let permite alterar o valor.
+
+const message = 'Hello, world! Iniciando estudos com JavaScript!';
+console.log(message);
